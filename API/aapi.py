5afb829fbbc9a29e346776836325c2fcad29a210
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2021-10-10 11:38:09 +0530

    API added
    Signed-off-by: Soham Banerjee <sohambanerjee4abc@hotmail.com>

---
+++ API/aapi.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+app = Flask(__name__)
+api = Api(app)
+
+
+class Users(Resource):
+    # methods go here
+    def get(self):
+        data = pd.read_csv('./API/users.csv')  # read CSV
+        data = data.to_dict()  # convert dataframe to dictionary
+        return {'data': data}, 200  # return data and 200 OK code
+        #return {'data': "works"}, 200  # return data and 200 OK code
+    pass
+
+
+class Locations(Resource):
+    def get(self):
+        data = pd.read_csv('./API/locations.csv')  # read CSV
+        data = data.to_dict()  # convert dataframe to dictionary
+        return {'data': data}, 200  # return data and 200 OK code
+        #return {'data': "works"}, 200  # return data and 200 OK code
+    pass
+
+
+api.add_resource(Users, '/users')  # '/users' is our entry point for Users
+api.add_resource(Locations, '/locations')  # and '/locations' is our entry point for Locations
+
+if __name__ == '__main__':
+    app.run()  # run our Flask app
