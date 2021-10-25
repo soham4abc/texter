e0e354d43028e6d722abf7c7895960a35aec98bb
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2021-10-25 20:23:49 +0530

    css added

--- Frontend/index.php
+++ Frontend/index.php
@@ -5,15 +5,11 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="styles.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <style>
-  .fakeimg {
-    height: 200px;
-    background: rgb(240, 194, 194);
-  }
-  </style>
+
 </head>
 <body>
 
@@ -81,10 +77,15 @@
     <input type="file" class="form-control-file" id="UnprocessedImage">
   </div>
 </form>
-<div class="jumbotron text-center" style="margin-bottom:0">
-  <p>Copyright 2021.</p>
-  <p>All rights reserved.</p>
-</div>
+
+<footer class="page-footer font-small bg-dark">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center text-light py-3 c">© 2021 Copyright:
+  </div>
+  <!-- Copyright -->
+
+</footer>
 
 <div>
   <script>
