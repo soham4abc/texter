be0fc17fc47f0db73efb0a96635211d4c9907803
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2021-12-11 10:40:40 +0530

    docx support added

--- Backend/test2.py
+++ Backend/test2.py
@@ -2,9 +2,9 @@ import base64
 from PIL import Image
 from io import BytesIO
 
-f = open('base64.txt', 'r')
+f = open("base64.txt", "r")
 data = f.read()
 f.closed
 
 im = Image.open(BytesIO(base64.b64decode(data)))
-im.save('image.png', 'PNG')
+im.save("image.png", "PNG")
