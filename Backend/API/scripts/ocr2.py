5d84c0e3113b4a5c8cf0d1d2ce639fefc9a59c79
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2022-02-13 23:00:34 +0530

    docx template added

--- Backend/API/scripts/ocr2.py
+++ Backend/API/scripts/ocr2.py
@@ -2,6 +2,7 @@ import cv2
 import os, argparse
 import pytesseract
 from PIL import Image
+import docx
 import aspose.words as aw
 
 # We then Construct an Argument Parser
@@ -20,10 +21,16 @@ def image_to_text(path):
     text = pytesseract.image_to_string(Image.open(filename))
     os.remove(filename)
     # print(text)
-    doc = aw.Document()
-    builder = aw.DocumentBuilder(doc)
-    builder.write(text)
-    doc.save("../../Frontend/documents/MyFile.docx")
+    # doc = aw.Document()
+    # builder = aw.DocumentBuilder(doc)
+    # builder.write(text)
+    # doc.save("../../Frontend/documents/MyFile.docx")
+    finaltext = "".join(c for c in text if valid_xml_char_ordinal(c))
+    mydoc = docx.Document()
+    # print(type(text))
+    mydoc.add_paragraph(finaltext)
+    mydoc.save("../../Frontend/documents/MyFile.docx")
+    # doc.save("../../Frontend/documents/MyFile.docx")
     # file1 = open("../../Frontend/documents/MyFile.txt", "w")
     # file1.write(text)
 
@@ -31,3 +38,14 @@ def image_to_text(path):
     # cv2.imshow("Image Input", images)
     # cv2.imshow("Output In Grayscale", gray)
     cv2.waitKey(0)
+
+
+def valid_xml_char_ordinal(c):
+    codepoint = ord(c)
+    # conditions ordered by presumed frequency
+    return (
+        0x20 <= codepoint <= 0xD7FF
+        or codepoint in (0x9, 0xA, 0xD)
+        or 0xE000 <= codepoint <= 0xFFFD
+        or 0x10000 <= codepoint <= 0x10FFFF
+    )
