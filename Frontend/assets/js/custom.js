a4fb9fe7ae47a361c8c96246fe75e92dd9b5ebfd
soham4abc <sohambanerjee4abc@hotmail.com>
2023-02-19 14:57:35 +0530

    server optimisation

--- Frontend/assets/js/custom.js
+++ Frontend/assets/js/custom.js
@@ -1,5 +1,5 @@
-//url="https://ocr-api.enbinary.com/"
-url="http://192.168.0.106:81/" //enter your backend url here
+url="https://ocr-api.enbinary.com/"
+//url="http://192.168.0.106:81/" //enter your backend url here
 $(document).ready(function() {
     
     // $(".chosen-select").chosen({
